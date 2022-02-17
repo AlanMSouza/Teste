571084568dc8bc69c657ab395844a99180b72f81
Alan M. Souza <alan.graia@hotmail.com>
2022-02-17 01:53:25 -0300

    alteracao

--- EXERCICIO 01 BD.sql
+++ EXERCICIO 01 BD.sql
@@ -22,7 +22,7 @@ email		varchar (30) not null,
 cpf			varchar	(13) not null,
 nome		varchar (30) not null,	
 senha		varchar (20) not null,
-datanasc	datetime	 not null,
+datanasc	datetime	 null,
 genero		char	(1)	 not null,
 telefone	varchar (20) not null,
 primary key (email, cpf, senha),
@@ -34,4 +34,18 @@ insert into cadastro values ('luiz@gmail.com', '29039482745', 'Luiz', 'luiz123',
 
 select * from cadastro;
 
+
+create table email (
+emailId		int				not null identity,
+email		varchar	(30)	not null,
+senha		varchar (20)	not null,
+pais		varchar (30)	not null,
+datanasc	datetime		null,
+)
+
+insert into email values ('igor@gmail.com', 'igor@123', 'Brasil', '10/12/1990');
+insert into email values ('jose@gmail.com', 'jose@123', 'Brasil', '09/01/2000');
+insert into email values ('hugo@gmail.com', 'hugo@123', 'Brasil', '09/09/2001');
+
+select * from email;
 
