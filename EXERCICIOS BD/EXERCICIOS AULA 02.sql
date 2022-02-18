cf136e14d18e308cbffb4516e7fa8567defec6b4
alan m <alan@gmail.com>
2022-02-17 21:47:15 -0300

    alteracao

--- EXERCICIOS BD/EXERCICIOS AULA 02.sql
+++ EXERCICIOS BD/EXERCICIOS AULA 02.sql
@@ -25,10 +25,24 @@ select * from alunos;
 -- TABELA 02 --
 create table produtos (
 produto	varchar (30)	not null,
-marca	varchar (20)	not null,
-cor		varchar	(20)	not null,
+marca	varchar (20)	null,
+cor		varchar	(20)	null,
 preco	decimal	(7,2)	not null,
 quant	int				not null,
 vtot	decimal	(7,2)	not null,
 dtfab	datetime		null,
-)
+);
+
+insert into produtos values ('Mouse', 'Dell', 'Preto', 22.99, 1, 22.99, '01/01/2021');
+insert into produtos values ('Teclado', 'Microsoft', 'Prata', 17.45, 2, 34.90, '02/01/2021');
+insert into produtos values ('HD', 'Wester Digital', null, 199.99, 3, 599.97, '03/01/2021');
+insert into produtos values ('Monitor', 'Philips', 'Branco', 459.00, 4, 1836.00, '04/01/2021');
+insert into produtos values	('Placa de Video', 'Sound Forge', null, 999.00, 5, 4995.00, '05/01/2021');
+insert into produtos values	('Mouse Pad', null, 'Azul', 15.00, 6, 90.00, '06/01/2021');
+
+select * from produtos;
+
+
+-- TABELA 03 --
+creat
+
